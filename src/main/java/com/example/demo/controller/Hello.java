5ec290d662c14f6cdc3994b4e7ef47db4ed0170d
gaoxing1103 <1446326581@qq.com>
2026-07-29 10:32:15 +0800

    修改输出

--- src/main/java/com/example/demo/controller/Hello.java
+++ src/main/java/com/example/demo/controller/Hello.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Hello {
     @RequestMapping("/hello")
     public String index(){
-        return "hello word123456";
+        return "hello word12345678";
     }
 
 }
